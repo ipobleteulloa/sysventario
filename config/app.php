377fd706669d407f6afdfc111d86f019d66c004d
ipobleteulloa <ipoblete@sanchezysanchez.cl>
2017-10-23 10:37:31 -0300

    Cambio de directorio de /laravel/inventario/ a /sysventario/

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/laravel/inventario/'),
+    'url' => env('APP_URL', 'http://localhost/sysventario/'),
 
     /*
     |--------------------------------------------------------------------------
